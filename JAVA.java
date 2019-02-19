d8dcd7d9a76ccee7ca5e53e14776375cfcf90e28
jbmerand <jbmerand@icloud.com>
2019-02-19 12:54:56 +0100

    initial commit

---
+++ JAVA.java
@@ -0,0 +1,61 @@
+/******************************************************************************/
+/************** DEFINITIONS ***************************************************/
+/******************************************************************************/
+
+/*
+- créateurs : James Grosling, Patrick Naughton
+- développé par Sun Microsystems (dès 1990), puis ORACLE (dès 2009)
+- langage le plus répandu dans entreprises (grosses applications)
+- outils riches, variés, matures, communauté ++
+- langage intermédiaire (permettant bonnes performances) :
+-- code pseudo-compilé (compilateur transforme code en ByteCode, binaire)
+-- ByteCode lut par interpréteur (JVM : JAVA Virtual Machine)
+- JDK : Java Developer Toolkit (compilateur, interpréteur JVM, JShell)
+-- JShell (dès Java 9) : outil pour tester code dans terminal
+- .jar : extension d'un programme (dossier archive contenant classes et config)
+*/
+
+/******************************************************************************/
+/************** ETAPES A SUIVRE, INFOS ****************************************/
+/******************************************************************************/
+
+/* 
+- IDE répandu : IntelliJ
+- un fichier = une partie logique de l'appli, contient classe portant même nom
+- fichiers séparés dans des packages (sous-dossiers)
+
+- dans terminal :
+-- connaitre version Java : javac -version
+-- compiler fichier .java : javac Fichier.java (sans réponse = sans erreur)
+-- exécuter programme : java Fichier
+-- saisir directement code java pour tester : jshell (CTRL+D pour quitter)
+
+- configurer variables environnement sur MacOS (pour changer version Java...) :
+-- modifier .bash_profile (/Library/Java/JavaVirtualMachines/...), ajouter :
+-- JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-9.0.1.jdk/Contents/Home/
+-- PATH=$JAVA_HOME/bin:$PATH
+
+- exporter programme : 
+-- File > Project Structure...
+-- dans partie Artifact : Add > JAR > From modules with dependencies
+-- renseigner classe principale du projet (Main Class)
+-- nouvel Artifact créé, puis Build > Build Artifacts...
+-- Nom:jar > Build
+
+*/
+
+
+// classe de base
+package com.jbmerand;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World !");
+    }
+}
+
+// écrire texte
+System.out.println("Hello World !");
+// out : permet d'accéder à sortie standard
+// println : afficher une ligne de texte
