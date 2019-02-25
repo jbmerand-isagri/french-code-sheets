626017d4c664570acf8e7e795cbd76c090fb7fd3
jbmerand <jbmerand@icloud.com>
2019-02-25 12:04:40 +0100

    few updates

--- JAVA.java
+++ JAVA.java
@@ -13,6 +13,7 @@
 - JDK : Java Developer Toolkit (compilateur, interpréteur JVM, JShell)
 -- JShell (dès Java 9) : outil pour tester code dans terminal
 - .jar : extension d'un programme (dossier archive contenant classes et config)
+- langage orienté objet au typage fort
 */
 
 /******************************************************************************/
@@ -44,18 +45,6 @@
 
 */
 
-// classe de base
-package fr.jbmerand;
-
-import com.sun.org.apache.xpath.internal.SourceTree;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    System.out.println("Hello World !");
-    }
-}
-
 /******************************************************************************/
 /************** IntelliJ ******************************************************/
 /******************************************************************************/
@@ -70,21 +59,44 @@ public class Main {
 // noms en camelCase
 
 /* types primitifs */
+byte // nombre, 1 byte
+short // nombre, 2 bytes
+int // nombre, 4 bytes
+long // nombre, 8 bytes
+float // nombre décimal, 4 bytes
+double // nombre décimal, 8 bytes
+char // un caractère, 2 bytes
+boolean // vrai ou faux, 1 byte
 
-int age = 28; // type Integer (peut : x + - %)
-double age2 = 28.5; // décimal (peut : x + - / %)
-boolean a = true;
+int monNombre;
+monNombre = 5;
+// ou
+int monNombre = 5;
+
+double d = 4.5;
+
+float f = 4.5f; /* ou */ float f = (float) 4.5;
+
+boolean b = false;
 
 /* types complexes (objets) */
-// faut voir comme objet de la vie réel, représente concepts
+// faut voir comme objet de la vie réelle, représente concepts
 // possède fonctionnalités et des composants
 
-String varText = new String("mon texte");
-String varText2 = "autre texte"; // raccourci syntaxique de String
+String // chaînes de caractères
+int[] arr // tableaux
+
+String s1 = new String("Texte");
+// crée un string avec un constructeur
+
+String s2 = "Autre texte";
+// utilise "" pour créer directement un string
 
 /******************************************************************************/
 /************** CLASSES *******************************************************/
 /******************************************************************************/
+
+// toute classe publique doit être déclarée dans un fichier portant son nom
 
 public class Main {
     public static void Main(String[] args) {
@@ -92,24 +104,61 @@ public class Main {
         order.runMenu();
     }
 }
+// la classe principale d'un programme
+
+// public : toutes les autres classes peuvent y accéder
+
+/******************************************************************************/
+/************** TABLEAUX ******************************************************/
+/******************************************************************************/
+
+// basé sur 0
+// nécessite une boucle pour afficher
+
+int[] tableau = new int[27];
+// crée tableau de 27 cases
+
+tableau[0] = 22;
+// associe valeur
+
+int[] tableau = {33, 22, 10, 4};
+// crée tableau en assignant valeurs
 
 
 /******************************************************************************/
 /************** FONCTIONS *****************************************************/
 /******************************************************************************/
 
+// ne peut retourner qu'une seule et unique valeur
+
 /**
- * Display a selected menu.
- * @param nbMenu The selected menu.
+ * Display a selected number.
+ * @param nombre The selected number.
  */
-public void displaySelectedMenu(int nbMenu) {
-    nbMenu = nbMenu + 5;
+public void displayNumber(int nombre) {
+    nombre = nombre + 5;
 }
+
+public static void {
+    // code
+}
+// static : exécutable depuis n'importe où, sans besoin d'instancier
+// void : ne retourne pas de valeurs
+// permet créer des boites à outils
 
 /******************************************************************************/
 /************** CONDITIONS ****************************************************/
 /******************************************************************************/
 // variable de type booléen créée
+
+String a = new String("texte");
+String b = new String("texte");
+String sameA = a;
+
+boolean r1 = a == b;      // false, puisque a et b ne sont pas le même objet
+boolean r2 = a.equals(b); // true, puisque a et b sont logiquement égaux
+boolean r3 = a == sameA;  // true, puisque a et sameA sont le même objet
+
 
 if (number == 50) {
     System.out.println("Le nombre est 50");
@@ -136,19 +185,35 @@ switch (maVariable) {
 /************** BOUCLES *******************************************************/
 /******************************************************************************/
 
-while (counter < 5) {
-    // code;
-    counter = counter + 1;
-}
-
-for (int counter = 0; counter < 5; counter = counter + 1) {
+while (condition) {
     // code;
 }
 
-do {
+do { // se lance au moins une fois
     // code;
-    i++;
-} while (i < 5);
+} while (condition);
+
+for (int i = 0; i < 4; i++) {
+    // à entrée boucle ; gate keeper (false fait quitter) ; à chaque exécution
+    // code;
+}
+
+break // stoppe et fait sortir de la boucle
+continue // stoppe l'itération actuelle et fait passer à la suivante
+
+/* pour foreach avec des tableaux */
+
+int[] arr = {1, 9, 9, 5};
+for (int i = 0; i < arr.length; i++) {
+    int el = arr[i];
+    System.out.println(el);
+}
+// ou version raccourcie
+int[] arr = {2, 0, 1, 3};
+for (int el : arr) {
+    System.out.println(el);
+}
+
 
 /******************************************************************************/
 /************** OPERATEURS ****************************************************/
@@ -164,12 +229,29 @@ do {
 && // ET
 || // OU
 
++=
+
 /******************************************************************************/
 /************** STRING ********************************************************/
 /******************************************************************************/
 
 varText.length(); // obtenir longueur de la chaine de caractères
 varText.contains("hello"); // bool, test si mot contenu dans variable
+
+
+/******************************************************************************/
+/************** ERREURS *******************************************************/
+/******************************************************************************/
+
+// Quand exception intervient, Java donne son type. Pour certaines, Java oblige
+// à faire un catch, sinon code ne compile pas.
+
+try {
+    // code susceptible générer exception
+} catch (ArithmeticException e) { // type exception, e donné arbitrairement
+    // code à excécuter si exception intervenue
+}
+
 
 /******************************************************************************/
 /************** TESTS *********************************************************/
@@ -192,10 +274,28 @@ public void Given_Nothing_When_DisplayMenuSelection_Then_ShouldDisplayText() {
     assertEquals(false, outContent.toString().isEmpty());
 }
 
+
+// A AJOUTER //
 assertEquals();
 toString();
 replace();
 split();
+.length
+contains()
+charAt()
+
+un scanner sc.
+
+this
+
+import
+import static
+
+/r /n;
+
+String.format("texte%n");
+
+// l'objet Math.
 
 System.out.println("Hello World !");
 // out : permet d'accéder à sortie standard
