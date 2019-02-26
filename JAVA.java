88e4b3d8b7837be47fa644b60c15f7952a2028f3
jbmerand <jbmerand@icloud.com>
2019-02-26 11:58:05 +0100

    basic concepts added

--- JAVA.java
+++ JAVA.java
@@ -21,9 +21,24 @@
 /******************************************************************************/
 
 /* 
+
+4 types de visibilité pour classes, méthodes :
+- public : toutes les autres classes peuvent y accéder
+- private : accessible uniquement dans classe
+- rien : accessible uniquement dans le package
+- protected : accessible dans classe et ses enfants
+
 - IDE répandu : IntelliJ
 - un fichier = une partie logique de l'appli, contient classe portant même nom
 - fichiers séparés dans des packages (sous-dossiers)
+
+- maven : gestionnaire de dépendances
+-- convertir en projet maven (clic droit sur projet, "add framework support")
+-- pom.xml fichier de configuration
+-- dépendance : ensemble de codes avec fonctionnalité précise
+--- librairie : fonctionnalité spécifique, se greffe facilement
+--- framework : beaucoup plus complet, change généralement organisation du code
+-- mvnrepository.com
 
 - dans terminal :
 -- connaitre version Java : javac -version
@@ -50,7 +65,11 @@
 /******************************************************************************/
 
 /*
+- code -> generate pour créer facilement getter, setter
 - ALT+ENTREE sur objet/outil : ajout ligne pour importer en haut fichier
+- psvm : raccourci pour créer rapidement méthode main
+- sout : raccourci pour System.out.println()
+*/
 
 /******************************************************************************/
 /************** VARIABLES *****************************************************/
@@ -58,7 +77,9 @@
 
 // noms en camelCase
 
-/* types primitifs */
+/*************** TYPES PRIMITIFS **********************************************/
+// moins lourds, meilleure performances
+
 byte // nombre, 1 byte
 short // nombre, 2 bytes
 int // nombre, 4 bytes
@@ -79,12 +100,19 @@ float f = 4.5f; /* ou */ float f = (float) 4.5;
 
 boolean b = false;
 
-/* types complexes (objets) */
+/*************** TYPES COMPLEXES (OBJETS) *************************************/
 // faut voir comme objet de la vie réelle, représente concepts
 // possède fonctionnalités et des composants
 
+Double
+Integer
+Boolean
+
 String // chaînes de caractères
 int[] arr // tableaux
+
+// List?
+// HashMap?
 
 String s1 = new String("Texte");
 // crée un string avec un constructeur
@@ -93,27 +121,113 @@ String s2 = "Autre texte";
 // utilise "" pour créer directement un string
 
 /******************************************************************************/
+/************** OBJETS ********************************************************/
+/******************************************************************************/
+
+// = variable d'un type d'une classe
+// constructeur : passe valeur à création objet, même nom que classe
+
+public class Personne {
+    private Age age;
+
+    public Personne(Age age) { // constructeur (pas besoin javadoc)
+        this.age = age;
+    }
+}
+
+/******************************************************************************/
 /************** CLASSES *******************************************************/
 /******************************************************************************/
 
+// = la description d'un type
 // toute classe publique doit être déclarée dans un fichier portant son nom
 
 public class Main {
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         Order order = new Order();
         order.runMenu();
     }
 }
 // la classe principale d'un programme
 
-// public : toutes les autres classes peuvent y accéder
+/* favoriser bonne visibilité du code */
+// par défaut, toujours mettre tous les attributs à private
+// si besoin lire ou utiliser informations depuis extérieur : créer getter
+// si besoin de le modifier depuis extérieur : créer setter
+// pas besoin javadoc pour getters et setters
+
+public class MaClass {
+    private int number; // attribut
+
+    public int getNumber() { // getter
+        return number;
+    }
+
+    public void setNumber(int number) { // setter
+        this.number = number;
+    }
+
+}
+
+/* héritage */
+
+// classe enfant prend toutes les fonctionnalités de son parent, spécialise
+
+public class Vehicle {
+    // attributs
+    private String modelName;
+    private int year;
+    private int[] dimensions = new int[3];
+}
+
+// Car hérite de Vehicle
+public class Car extends Vehicle {
+    private int door;
+    private String motor;
+    private List<String> options = new ArrayList<String>();
+
+    // constructeur
+    public Car(String modelName, int year, int[] dimensions, int door, String motor, List<String> Options) { 
+        super(modelName, year, dimensions); // reprend classe mère
+        this.door = door;
+        this.motor = motor;
+        this.options = options;
+    }
+}
+
+/* généralisation */
+
+private Map<Product, Integer> products = new HashMap<Product, Integer>();
+// peut ainsi facilement changer le type vers un autre type (enfant)
+
+/* surcharger fonctionnalités */
+@Override
+public void maMéthode() { // prendre signature identique
+    // code différent
+}
+
+/* classe abstraite */
+
+public abstract class MaClasse { // ininstanciable
+    
+    public abstract void maMéthode(); // surcharge obligatoire pour instancier
+}
+
+
+/* DIVERS
+- Writer : une classe capable d'écrire dans un fichier.
+- InnerClasses : classes créées dans d'autres.
+- classes anonymes : héritage dans le code, comportement de base changé, sans
+réellement créer une nouvelle classe avec un nom.
+Mock?
+
 
 /******************************************************************************/
 /************** TABLEAUX ******************************************************/
 /******************************************************************************/
 
-// basé sur 0
-// nécessite une boucle pour afficher
+// basé sur 0, nécessite boucle pour afficher, taille non augmentable
+// aucune fonctionnalité livrée directement avec
 
 int[] tableau = new int[27];
 // crée tableau de 27 cases
@@ -124,6 +238,44 @@ tableau[0] = 22;
 int[] tableau = {33, 22, 10, 4};
 // crée tableau en assignant valeurs
 
+
+/******************************************************************************/
+/************** LISTES ********************************************************/
+/******************************************************************************/
+
+List<String> names = new ArrayList<String>();
+// ArrayList est une classe de type générique (peut fonctionner avec plusieurs
+// autres types et doit donc le préciser quand on le crée)
+
+names.add("Ashe");
+// bool, ajoute valeur
+
+names.get(0);
+// retourne valeur selon index
+
+names.indexOf("Ashe");
+// retourne index de la valeur
+
+names.contains("Ashe");
+// bool, indique si valeur présente dans la liste
+
+names.remove("Ashe");
+// bool, supprime valeur de la liste
+
+Map<String, Integer> ages = new HashMap<String, Integer>();
+// HashMap sert à stocker couples de clés et de valeurs
+
+ages.put("John, 44");
+// ajoute valeur ou la met à jour si existante
+
+ages.containsKey("John");
+// bool, indique si clé présente
+
+ages.get("John");
+// retourne valeur
+
+ages.remove("John");
+// retourne sa valeur, supprime
 
 /******************************************************************************/
 /************** FONCTIONS *****************************************************/
@@ -182,6 +334,27 @@ switch (maVariable) {
 
 
 /******************************************************************************/
+/************** INTERFACES ****************************************************/
+/******************************************************************************/
+
+/* s'intéresse aux entrées et aux sorties
+- ressemble classe abstraite sans comportement intérieur (attribut, méthode)
+= un contrat : donne le besoin, le comportement attendu, à implémenter
+*/
+
+public interface MoyenDeLocomotion { // création interface (non instanciable)
+    void deplace(String adresse); // ajoute pas de contenu
+}
+
+public class Taxi implements MoyenDeLocomotion { // classe à partir interface
+    
+    @Override
+    public void deplace(String adresse) {
+        System.out.println("Je suis un taxi et je vais à " + adresse);
+    }
+}
+
+/******************************************************************************/
 /************** BOUCLES *******************************************************/
 /******************************************************************************/
 
@@ -236,7 +409,7 @@ for (int el : arr) {
 /******************************************************************************/
 
 varText.length(); // obtenir longueur de la chaine de caractères
-varText.contains("hello"); // bool, test si mot contenu dans variable
+varText.contains("hello"); // bool, teste si mot contenu dans variable
 
 
 /******************************************************************************/
@@ -265,6 +438,13 @@ TDD (Test Driven Development) : pense au test avant le développement du code.
 -- exécuter les tests et vérifier qu'aucun ne fonctionne
 -- écrire le code de la fonction
 -- exécuter à nouveau les tests pour vérifier tout fonctionne
+
+Etapes
+- créer dossier "test" -> clic droit IntelliJ pour préciser dédié aux tests
+- générer automatiquement classe de test : Navigate -> Test
+- utiliser junitfile
+- ajouter une classe de test dès qu'on ajoute une nouvelle classe
+
 */
 
 // format Given_ParamètresDuTest_When_ContexteOùEst_Then_RésultatAttendu
@@ -275,14 +455,33 @@ public void Given_Nothing_When_DisplayMenuSelection_Then_ShouldDisplayText() {
 }
 
 
+/******************************************************************************/
+/************** FICHIER CSV ***************************************************/
+/******************************************************************************/
+
+// CSV : Comma-Separated Values (fichier texte, pour stocker infos)
+
+Path nom = Paths.get("fichier.csv");
+// crée type path
+
+Files.write(nom, String.format("texte").getBytes(), APPEND);
+// peut déclancher une exception si fichier n'existe pas (à catcher)
+
+lines.get(2.).split();
+
 // A AJOUTER //
+List<String>
 assertEquals();
 toString();
 replace();
 split();
 .length
+isEmpty();
+trim();
 contains()
 charAt()
+
+Integer.valueOf()
 
 un scanner sc.
 
