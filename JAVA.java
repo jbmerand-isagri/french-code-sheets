020c2d4e4f4c34a164a419f6c78e33f48afdeafd
jbmerand <jbmerand@icloud.com>
2019-03-11 12:06:51 +0100

    major add on main classes/methods

--- JAVA.java
+++ JAVA.java
@@ -72,31 +72,116 @@
 */
 
 /******************************************************************************/
+/************** CONCEPTS POO **************************************************/
+/******************************************************************************/
+
+/* 
+OBJET
+Consiste en un état et un comportement associé, comme les objets du quotidien.
+Objet stocke son état dans des champs (variables) et expose son comportement
+par des méthodes (fonctions).
+Méthodes fonctionnent sur l'état interne d'un objet et servent de mécanisme principal pour communication entre objets.
+Encapsulation : cacher état interne et exiger que toute les interactions soient
+effectuées par des méthodes d'un objet.
+Intérêts :
+- modularité : code source peut être écrit et maintenu indépendamment
+- informations cachées : détails implémentation interne cachés de l'extérieur
+- réutilisation : pour implémenter, tester, déboguer objets complexes, spécifiques
+- pluggabilité et facilité de débogage
+
+CLASSE
+= le modèle à partir duquel des objets individuels sont créés
+
+HERITAGE
+Classes héritent des états et comportements des autres classes.
+Chaque classe autorisée à avoir une superclasse directe.
+Chaque superclasse peut avoir un nombre illimité de sous-classes.
+
+INTERFACE
+Implémentation interface permet à une classe devenir plus formelle quant au
+comportement qu'elle promet de fournir.
+Interfaces forment contrat entre classe et monde extérieur, qui est appliqué au
+moment de la construction par le compilateur.
+
+PACKAGE
+= un namespace qui organise un ensemble de classes et d'interfaces connexes.
+JAVA fournit énorme bibliothèque de classes (ensemble de packages) = API.
+cf. Java Platform API Specification pour avoir le détail packages... :
+https://docs.oracle.com/javase/8/docs/api/index.html
+*/
+
+
+/******************************************************************************/
 /************** VARIABLES *****************************************************/
 /******************************************************************************/
 
-/*************** TYPES PRIMITIFS **********************************************/
-// moins lourds, meilleure performances
+/***** VARIABLES D'INSTANCE (CHAMPS NON STATIQUES) *****/
 
-byte // nombre, 1 byte
-short // nombre, 2 bytes
-int // nombre, 4 bytes
-long // nombre, 8 bytes
-float // nombre décimal, 4 bytes
-double // nombre décimal, 8 bytes
-char // un caractère, 2 bytes
-boolean // vrai ou faux, 1 byte
+// objets stockent leurs états individuels dans champs non statiques
+// leurs valeurs sont uniques à chaque instance d'une classe (chaque objet)
+
+/***** VARIABLES DE CLASSE (CHAMPS STATIQUES) *****/
+
+// = champs déclaré avec modificateur static (indique compilateur existe 
+// exactement une copie cette variable, quelque soit nombre fois classe
+// instanciée)
+
+/***** VARIABLES LOCALES *****/
+
+// méthode stocke souvent son état temporaire dans variables locales
+// syntaxe de déclaration = celle d'un champs (pas mot-clé spécial)
+// détermination provient déclarée entre accolades ouvrantes/fermantes méthode
+// visible que par méthode dans laquelle est déclarée
+// non accessibles à partir du reste de la classe
+
+/***** PARAMETRES *****/
+
+// toujours classés en variables et non en champs
+
+/*************** NOMMAGE ******************************************************/
+
+// peut commencer par lettre, $ (à proscrire) ou _ (à éviter)
+// utiliser mots entiers, différents de mots réservés ou mots-clés
+static final int NUM_GEARS = 6 // nom valeur constante (majuscule, avec _)
+int gearRatio // camelCase sinon
+
+/*************** TYPES DE DOONNEES PRIMITIFS **********************************/
+// moins lourds, meilleures performances
+// langage au typage statique => variables doivent être déclarées avec leur
+// type et leur nom avant d'être utilisées
+// classe String pas techniquement ce type, mais peut être pensée comme tel
+// compilateur définit valeur par défaut champs déclarés mais non initialisés,
+// sauf pour variables locales
+
+int gear = 1;
+// champ "gear" existe, contient données numériques et a valeur initiale de 1
+
+// 8 types de données primitives / valeur par défaut pour champs:
+byte // nombre, -128 à 127 inclus, 8-bit / 0
+short // nombre, -32768 à 32767 inclus, 16-bit / 0
+int // nombre, -2^31 à 2^31-1, 32-bit / 0
+long // nombre, -2^63 à 2^63-1, 64-bit / 0L
+float // nombre décimal, 32-bit => pour sauver mémoire dans tableaux / 0.0f
+double // nombre décimal, 64-bit => choix généralement par défaut / 0.0d
+// float/double jamais pour valeurs précises, utiliser classes Numbers/Strings
+char // un caractère, 16-bit / 'u\0000'
+boolean // deux valeurs possibles : true ou false / false
 
 int monNombre;
-monNombre = 5;
+monNombre = 5_2; // peut utiliser _ pour nombre entre chiffres
 // ou
 int monNombre = 5;
-
 double d = 4.5;
-
 float f = 4.5f; /* ou */ float f = (float) 4.5;
-
 boolean b = false;
+
+/** LITTERAUX **/
+
+// un littéral est la représentation du code source d'une valeur fixe
+// possible affecter littéral à variable de type primitif
+// littéraux char et String : '' pour char, "" pour String
+// \b backspace \t tab \n line feed \f form feed \r carriage return \\ backslash
+
 
 /*************** TYPES COMPLEXES (OBJETS) *************************************/
 // faut voir comme objet de la vie réelle, représente concepts
@@ -106,7 +191,7 @@ Double
 Integer
 Boolean
 
-String // chaînes de caractères
+String // chaînes de caractères / null
 int[] arr // tableaux
 
 // List?
@@ -329,10 +414,10 @@ getBytes([Charset charset] [String charsetName])
 // byte[]; encode en une séquence d'octets avec jeu de crc par défaut/défini
 // stocke résultat dans un nouveau tableau d'octets
 
-getChars(int srcBegin, int srcEnd, char[] dst, int dstBegin)
+getChars(int srcBegin, int srcEnd, char[] dst, int dstBegin);
 // void; copie les crc du string dans le tableau de crc
 
-indexOf(int ch [String str] [String str, int fromIndex])
+indexOf(int ch [String str] [String str, int fromIndex]);
 // int; retourne index dans string première occurence crc/string
 
 intern() // String; retourne expression canonique
@@ -349,7 +434,7 @@ length()
 matches(String regex)
 // boolean; indique si chaine correspond à expression régulière
 
-offsetByCodePoints(int index, int codePointOffset) // int
+offsetByCodePoints(int index, int codePointOffset); // int
 regionMatches(boolean ignoreCase, int toffset, String other, int ooffset, int len)
 
 replace(char oldChar, char newChar[,CharSequence targ, CharSequence replac])
@@ -367,16 +452,16 @@ split(String regex [, int limit])
 startsWith(String prefix [, int toffset])
 // boolean; teste si chaine (sous-chaine dès index) commence par préfixe
 
-subSequence(int beginIndex, int endIndex)
+subSequence(int beginIndex, int endIndex);
 // CharSequence; retourne nouvelle séquence crc qui est une partie séquence
 
-substring(int beginIndex [, int endIndex])
+substring(int beginIndex [, int endIndex]);
 // String; retourne nouvelle chaune qui est sous-chaine de la chaine
 
 toCharArray()
 // char[]; convertit chaine en un nouveau tableau de crc
 
-toLowerCase([Locale locale])
+toLowerCase([Locale locale]);
 // String; convertit tous crc en minuscule, suivant règles locales / définies
 
 toUpperCase([Locale locale])
@@ -393,59 +478,301 @@ valueOf(char[] data, int offset, int count)
 
 
 /******************************************************************************/
-/************** TABLEAUX ******************************************************/
+/************** CLASS ARRAYS (TABLEAUX) ***************************************/
 /******************************************************************************/
+
+// java.lang.Object > java.util.Arrays // JDK 1.2
 
 // basé sur 0, nécessite boucle pour afficher, taille non augmentable
 // aucune fonctionnalité livrée directement avec
 
+int[] unTableau;
+// déclare un tableau d'entiers
+
 int[] tableau = new int[27];
-// crée tableau de 27 cases
+// crée tableau d'entiers de 27 éléments
 
 tableau[0] = 22;
-// associe valeur
+// associe valeur / initialise premier élément
 
 int[] tableau = {33, 22, 10, 4};
-// crée tableau en assignant valeurs
+// syntaxe raccourcie pour créer et initialiser
+
+class MultiDimArray {
+    public static void main(String[] args) {
+        String[][] names = { // tableau multidimensionnel
+            {"Mr. ", "Mrs. ", "Ms. "},
+            {"Smith", "Jones"}
+        };
+        // Mr. Smith
+        System.out.println(names[0][0] + names[1][0]);
+        // Ms. Jones
+        System.out.println(names[0][2] + names[1][1]);
+    }
+}
+
+asList(T... a)
+// static <T> List<T>; retourne liste taille fixe sauvegardée par tableau
+
+binarySearch(int[] a, int key)
+binarySearch(int[] a, int fromIndex, int toIndex, int key)
+// static int; recherche valeur spécifiée ds tableau avec algo recherche binaire
+// ou byte[], char[], double[], float[], long[], short[], Object[]
+// binarySearch(T[] a, T key, Comparator<? super T> c)
+// binarySearch(T[] a, int fromIndex, int toIndex, T key, Comparator<? super T> c)
+// static <T> int; recherche objet spécifié dans tableau
+
+copyOf(boolean[] original, int newLength)
+// static boolean[]; copie tableau en tronquant ou en remplissant avec false 
+// (zero/null) si nécessaire pour que copie ait longueur spécifiée
+// ou byte[], char[], double[], float[], int[], long[], short[], T[]
+// copyOf(U[] original, int newLength, Class<? extends T[]> newType)
+// static <T,U> T[]
+
+copyOfRange(boolean[] original, int from, int to)
+// static boolean[]; copie la plage spécifiée du tableau dans un nouveau tableau
+// ou byte[], char[], double[], float[], int[], long[], short[], T[]
+// copyOfRange(U[] original, int from, int to, Class<? extends T[]> newType)
+// static <T,U> T[]
+
+deepEquals(Object[] a1, Object[] a2)
+// static boolean; true si les deux tableaux sont profondément égaux
+
+deepHashCode(Object[] a)
+// static int; retourne code hashé basé sur le "contenu profond" du tableau
+
+deepToString(Object[] a)
+// static String; retourne représentation string du "contenu profond" du tableau
+
+equals(boolean[] a, boolean[] a2)
+// static boolean; true si les deux tableaux sont égaux
+// ou byte[], char[], double[], float[], int[], long[], Object[], short[]
+
+fill(boolean[] a, boolean val)
+// static void; assigne valeur booléenne spécifiée à chaque élément tableau
+// ou byte[], char[], double[], float[], int[], long[], Object[], short[]
+
+fill(boolean[] a, int fromIndex, int toIndex, boolean val)
+// static void; assigne valeur booléenne spécifiée à chaque élément de la plage
+// ou byte[], char[], double[], float[], int[], long[], Object[], short[]
+
+hashCode(boolean[] a)
+// static int; retourne code hashé basé sur contenu du tableau
+// boolean remplaçable (byte, char, double...)
+
+parallelPrefix(double[] array, DoubleBinaryOperator op)
+parallelPrefix(double[] array, int fromIndex, int toIndex, DoubleBinOpe op)
+// static void; cumule, en parallèle, chaque élément tableau, utilisant fonction
+// ou int[], long[], T[]
+
+parallelSetAll(double[] array, IntToDoubleFunction generator)
+// static void; définit tous éléments tableau avec fonction de génération
+// ou int[], long[]
+// parallelSetAll(T[] array, IntFunction<? extends T> generator)
+
+parallelSort(byte[] a)
+parallelSort(byte[] a, int fromIndex, int toIndex)
+// static void; trie tableau en ordre numérique croissant
+// ou char[], double[], float[], int[], long[], short[]
+// parallelSort(T[] a, Comparator<? super T> cmp)
+
+setAll(double[] array, IntToDoubleFunction generator)
+// static void; définit éléments tableau en utilisant fonction génération
+// ou int[], long[], T[]
+
+sort(byte[] a)
+sort(byte[] a, int fromIndex, int toIndex)
+// static void; trie tableau dans ordre croissant
+// ou char[], double[], float[], int[], long[], Object[], short[]
+// sort(T[] a, Comparator<? super T> c)
+// sort(T[] a, int fromIndex, int toIndex, Comparator<? super T> c)
+
+spliterator(double[] array)
+spliterator(double[] array, int startInclusive, int endExclusive)
+// static Spliterator.OfDouble; retourne un Spliterator.OfDouble
+// ou int[], long[], T[]
+
+stream(double[] array)
+stream(double[] array, int startInclusive, int endExclusive)
+// static DoubleStream; retourne séquence DoubleStream avec tableau pour source
+// static DoubleStream; couvre plage spécifiée
+// ou int[], long[], T[]
+
+toString(boolean[] a)
+// static String; retourne représentation string du contenu
+// ou byte[], char[], double[], float[], int[], long[], Object[], short[]
+
+/******************************************************************************/
+/************** INTERFACE ITERABLE<T> *****************************************/
+/******************************************************************************/
+
+// java.lang / 1.5
+// son implémentation permet objet être cible instruction "for-each loop"
+
+public interface Iterable<T> {}
+
+forEach(Consumer<? super T> action)
+// default void; effectue action donnée pour chaque élément de l'Iterable
+// jusqu'à ce que tous éléments traités ou que action envoie une exception
+
+iterator()
+// Iterator<T>; retourne un itérateur sur éléments de type T
+
+spliterator()
+// default Spliterator<T>; crée Spliterator sur éléments décrits par Iterable
 
 
 /******************************************************************************/
-/************** LISTES ********************************************************/
+/************** INTERFACE COLLECTION<E> ***************************************/
 /******************************************************************************/
+
+// java.util / 1.2
+// = interface racine dans hiérarchie de la collection
+// collection = groupe d'objets, appelés éléments
+// JDK fournit aucune implémentation directe cette interface, mais des
+// implémentation de sous-interfaces plus spécifiques (Set, List)
+// utilisée pour passer collections et les manipuler avec généralité maximale
+// pour Bags et multisets (collections non ordonnées avec éléments dupliqués)
+
+public interface Collection<E>
+extends Iterable<E> {}
+
+// pour détails, cf. LIST, SET
+
+add(E e) // boolean
+addAll(Collection<? extends E> c) // boolean
+clear() // void
+contains(Object o) // boolean
+containsAll(Collection<?> c) // boolean
+equals(Object o) // boolean
+forEach(Consumer<? super T> action) // héritée de java.lang.Iterable
+hashCode() // int
+isEmpty() // boolean
+iterator() // Iterator<E>
+parallelStream() // default Stream<E>
+remove(Object o) // boolean
+removeAll(Collection<?> c) // boolean
+
+removeIf(Predicate<? super E> filter)
+// default boolean; supprime tous éléments collection satisfaisant prédicat
+
+retainAll(Collection<?> c) // boolean
+size() // int
+spliterator() // default Spliterator<E>
+stream() // default Stream<E>
+toArray() // Object[]
+toArray(T[] a) // <T> T[]
+
+
+/******************************************************************************/
+/************** INTERFACE LIST<E>  ********************************************/
+/******************************************************************************/
+
+// = collection ordonnée (séquence)
+// contrôle précis sur chaque élément inséré, accès par index, recherche
+// autorise généralement éléments en double, éléments null (!= set (ensemble))
+// ajoute stipulations, autres que celles de l'interface Collection
+
+// Superinterfaces: Collection<E>, Iterable<E>
+// Implementing Classes: AbstractList, AbstractSequentialList, ArrayList, 
+// AttributeList, CopyOnWriteArrayList, LinkedList, RoleList, 
+// RoleUnresolvedList, Stack, Vector
+
+public interface List<E>
+extends Collection<E> {}
 
 List<String> names = new ArrayList<String>();
 // ArrayList est une classe de type générique (peut fonctionner avec plusieurs
 // autres types et doit donc le préciser quand on le crée)
 
-names.add("Ashe");
-// bool, ajoute valeur
+// méthodes héritées
+forEach() // java.lang.Iterable
+parallelStream(), removeIf(), stream() // java.util.Collection
 
-names.get(0);
-// retourne valeur selon index
+add(E el);
+add(int index, E element)
+// boolean, ajoute élément en fin de liste
+// void; ajoute à la position spécifiée
 
-names.indexOf("Ashe");
-// retourne index de la valeur
+addAll(Collection<? extends E> c)
+addAll(int index, Collection<? extends E> c)
+// boolean; ajoute tous éléments dans la collection à fin de la liste
 
-names.contains("Ashe");
-// bool, indique si valeur présente dans la liste
+clear()
+// void; efface tous éléments liste
 
-names.remove("Ashe");
-// bool, supprime valeur de la liste
+contains(Object o)
+// boolean; true si liste contient élément spécifié
 
-Map<String, Integer> ages = new HashMap<String, Integer>();
-// HashMap sert à stocker couples de clés et de valeurs
+containsAll(Collection<?> c)
+// boolean; true si liste contient tous éléments spécifiés de la collection
 
-ages.put("John, 44");
-// ajoute valeur ou la met à jour si existante
+equals(Object o)
+// boolean; compare objet avec liste pour égalité
 
-ages.containsKey("John");
-// bool, indique si clé présente
+get(int index)
+// E; retourne élément à position spécifiée
 
-ages.get("John");
-// retourne valeur
+hashCode()
 
-ages.remove("John");
-// retourne sa valeur, supprime
+indexOf(Object o)
+// int; retourne index de première occurence élément / -1 sinon
+
+isEmpty()
+// boolean; true si liste contient aucun élément
+
+iterator()
+// Iterator<E>; retourne itérateur sur éléments cette liste dans ordre approprié
+
+lastIndexOf(Object o)
+// int; retourne index dernière occurence élément / -1 sinon
+
+listIterator()
+listIterator(int fromIndex)
+// ListIterator<E>; retourne un itérateur de liste sur éléments cette liste
+
+remove(int index)
+// E; supprime élément à position spécifiée
+
+remove(Object o)
+// boolean; supprime première occurence élément spécifié, si présent
+
+removeAll(Collection<?> c)
+// boolean; supprime tous éléments contenus dans collection spécifiée
+
+replaceAll(UnaryOperator<E> operator)
+// default void; remplace chaque élément par résultat application de l'opérateur
+
+retainAll(Collection<?> c)
+// boolean; conserve uniquement élément contenus dans collection spécifiée
+
+set(int index, E element)
+// E; remplace élément à position spécifiée par élément spécifié
+
+size()
+// int; retourne nombre d'éléments
+
+sort(Comparator<? super E> c)
+// default void; trie la liste en fonction ordre induit par comparateur
+
+spliterator()
+// default Spliterator<E>; crée un Spliterator sur éléments de la liste
+
+subList(int fromIndexInclusive, int toIndexExclusive)
+// List<E>; retourne une vue de la partie de la liste
+
+toArray()
+toArray(T[] a)
+// Object[]; retourne tableau contenant tous éléments liste dans ordre approprié
+
+
+/******************************************************************************/
+/************** INTERFACE SET<E>  *********************************************/
+/******************************************************************************/
+
+// FIXME:
+
+https://docs.oracle.com/javase/8/docs/api/java/util/Set.html
 
 /******************************************************************************/
 /************** FONCTIONS *****************************************************/
@@ -648,6 +975,7 @@ split();
 isEmpty();
 trim();
 contains()
+// mot clé final
 
 Integer.valueOf()
 
